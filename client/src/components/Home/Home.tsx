76d6c1054599b89b99db59608f62ea68c7577e0c
KonImperator <nikola_hristov22@abv.bg>
2023-12-19 15:11:20 +0200

    Basic text post visual

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -6,8 +6,12 @@ export default function Home() {
         <>
             <section className="send-post">
                 <form className="send-post-form">
-                    <input type="text" placeholder="What's on your mind?" />
-                    <button>Post</button>
+                    <div className="send-post-input-container">
+                        <img className="send-post-img user-avatar" src="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="avatar" />
+                        <textarea className="send-post-input" placeholder="What's on your mind?" />
+                        {/* Need to add image uploads around here somewhere later */}
+                    </div>
+                    <button className="send-post-btn">Post</button>
                 </form>
             </section>
             <section className="posts-container">
